2c3893c71b825907282ca06656f19ba0436387c0
Douglas Suárez Zamorano <d.suarez.zamorano@gmail.com>
2025-08-08 22:00:36 -0400

    finalizacion del trabajo por hoy DSZ / 2025-08-08 22:00

--- main.py
+++ main.py
@@ -26,16 +26,14 @@ def mostrar_menu_principal():
             elif opcion == '2':
                 inventario.ver_inventario()
             
-            elif opcion == '3':
-                #leer datos de un producto especifico()
+            elif opcion == '3': #leer datos de un producto especifico()
                 busqueda = input ("Ingresar producto a buscar")
                 print = ("El producto es: ")
-
-            elif opcion == '4':
-                #modificar datos de un producto() 
+                # falta agregar esta función, para quien la quiera hacer
+            elif opcion == '4': #modificar datos de un producto() 
                 inventario.modificar_producto()
-            elif opcion == '5':
-                #eliminar un producto del inventario()
+            elif opcion == '5': #eliminar un producto del inventario()
+                # Falta agregar esta opción, para quien la quiera hacer
                 pass
             elif opcion == '6':
                 print("¡Hasta luego!")
