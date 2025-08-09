f34d2cac1746ad02e0305f3e16a07884780cb365
Douglas Suárez Zamorano <d.suarez.zamorano@gmail.com>
2025-08-08 21:58:17 -0400

    tercer commit

--- main.py
+++ main.py
@@ -27,15 +27,16 @@ def mostrar_menu_principal():
                 inventario.ver_inventario()
             
             elif opcion == '3':
-                leer datos de un producto especifico()
+                #leer datos de un producto especifico()
                 busqueda = input ("Ingresar producto a buscar")
                 print = ("El producto es: ")
 
             elif opcion == '4':
-                modificar datos de un producto() 
+                #modificar datos de un producto() 
                 inventario.modificar_producto()
             elif opcion == '5':
-                eliminar un producto del inventario()    
+                #eliminar un producto del inventario()
+                pass
             elif opcion == '6':
                 print("¡Hasta luego!")
                 break
